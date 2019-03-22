9501cc3d1c3731ba35871e7add25e1acfaccfc5f
sport <sport@LAPTOP-UNPD411Q>
2019-03-22 14:01:27 -0600

    clase pelicula terminada.

---
+++ Cinema/Cinema/modelo/Cartelera.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.modelo
+{
+    class Cartelera
+    {
+        private int _id;
+        private DateTime _fecha;
+        private string _horario;
+        private string _tipo;
+        private double _costo;
+
+        //Métodos Getter´s y Setter´s
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Horario { get; set; }
+        public string Tipo { get; set; }
+        public double Costo { get; set; }
+
+    }
+}
---
+++ Cinema/Cinema/modelo/Pelicula.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.modelo
+{
+    class Pelicula
+    {
+        private int _id;
+        private string _nombre;
+        private string _director;
+        private string _clasificacion;
+        private string _resumen;
+
+        //Métodos Getter´s y Setter´s
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Director { get; set; }
+        public string Clasificacion { get; set; }
+        public string Resumen { get; set; }
+    }
+}
