97a083177063fd2de50bfd870b8c21ec225beb62
sport <sport@LAPTOP-UNPD411Q>
2019-04-01 11:11:26 -0600

    vincular con bloc de notas para guardar y recuperar datos

---
+++ Partido/Partido/campeonato/FormPrincipal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Partido.campeonato
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
